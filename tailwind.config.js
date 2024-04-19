a71ec63694e64063b7cc0f61e960ec2faa993eb8
Harry Nguyen <hainguyen27798@gmail.com>
2024-04-19 22:42:37 +0700

    portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["index.html"],
+    theme: {
+        extend: {
+            colors: {
+                water: "#D1D5DB",
+                midnight: "#1F2937",
+                "alice-blue": "#F9FAFB",
+                "black-russian": "#111827",
+            },
+        },
+    },
+    plugins: [],
+};
